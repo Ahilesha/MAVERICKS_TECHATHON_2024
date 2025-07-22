0daeda33390efdec06324582f65b7394a9f7a557
Ahilesha <23i303@psgtech.ac.in>
2025-07-23 02:25:53 +0530

    Update store_user.py

--- store_user.py
+++ store_user.py
@@ -1,15 +1,11 @@
 import sqlite3
 import hashlib
-
 def hash_password(password):
     return hashlib.sha256(password.encode()).hexdigest()
-
 def store_user(username, password):
     conn = sqlite3.connect('users.db')
     cursor = conn.cursor()
-    
     hashed_password = hash_password(password)
-    
     try:
         cursor.execute('''
         INSERT INTO users (username, password) VALUES (?, ?)
@@ -17,8 +13,5 @@ def store_user(username, password):
         conn.commit()
     except sqlite3.IntegrityError:
         print("Username already exists.")
-    
     conn.close()
-
-# Example usage
 store_user('example_user', 'example_password')
